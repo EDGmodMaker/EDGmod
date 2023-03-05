ba897a6e32ec1b401feb1ec31d59edc695327b6c
DESKTOP-0BPKNST\p'c <1752805387@qq.com>
2023-03-05 16:21:11 +0800

    Update 1

---
+++ common/defines/gme.lua
@@ -0,0 +1,12 @@
+-- Format for overwriting define values:
+--
+-- NDefines.NDiplomacy.MAX_CLIENT_STATES = 20
+
+NDefines.NDiplomacy.GREAT_PROJECT_WARSCORE_COST_MODIFIER = 0.02
+
+NDefines.NDiplomacy.MAX_FREE_CITIES = 12
+NDefines.NDiplomacy.IMPERIAL_CITY_IA = 0.010						-- Monthly per imperial free city
+
+NDefines.NDiplomacy.IMPERIAL_AUTHORITY_FROM_PRINCES = 0.075
+
+MAX_CHRISTIAN_RELIGIOUS_CENTERS = 5
